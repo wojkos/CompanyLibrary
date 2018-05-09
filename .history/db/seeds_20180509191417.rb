0fd75f4aa01c4e7d38445382192a9a62ac5c9545
wojtek <wojtek@tappest.com>
2018-05-09 23:04:10 +0200

    .gitignore is now working

---
+++ .history/db/seeds_20180509191417.rb
@@ -0,0 +1,15 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+50.times do |i|
+	Book.create!(
+		tittle: Faker::Book.title,
+        author: Faker::Book.author,
+        category: Faker::Book.genre
+		)
+end
